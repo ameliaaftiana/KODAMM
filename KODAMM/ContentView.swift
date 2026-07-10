df5b187671cb8d0dc445a7ad430f57c13d421866
Amelia Putri Aftiana <117893334+ameliaaftiana@users.noreply.github.com>
2026-07-11 01:04:50 +0700

    Initial Commit

---
+++ KODAMM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KODAMM
+//
+//  Created by Amelia Putri Aftiana on 11/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KODAMM/KODAMMApp.swift
@@ -0,0 +1,17 @@
+//
+//  KODAMMApp.swift
+//  KODAMM
+//
+//  Created by Amelia Putri Aftiana on 11/07/26.
+//
+
+import SwiftUI
+
+@main
+struct KODAMMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
